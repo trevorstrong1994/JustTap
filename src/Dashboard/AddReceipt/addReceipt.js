a9dd8dd8c9237024e602671c229c244774eac735
LAPTOP-2P59BSNP\trevo <trevorstrong94@gmail.com>
2018-06-12 16:44:58 +0100

    Added all related Receipt files

---
+++ src/Dashboard/AddReceipt/addReceipt.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { StyleSheet, Platform, Image, Text, View, ScrollView, Button } from 'react-native';
+
+class AddExpenseScreen extends Component {
+    render() {
+        return(
+            <View>
+                <Text> ADD EXPENSE </Text>
+            </View>
+        );
+    }
+}
+
+export default AddExpenseScreen;
